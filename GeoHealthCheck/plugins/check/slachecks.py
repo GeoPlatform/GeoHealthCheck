aea2513172399b2616c5909997182116df113c47
Forest Gafford <forestg@imagemattersllc.com>
2017-09-14 16:45:27 -0400

    tmp: part way through moving request call to Probe

--- GeoHealthCheck/plugins/check/slachecks.py
+++ GeoHealthCheck/plugins/check/slachecks.py
@@ -1,6 +1,3 @@
-import sys
-
-from pyquery import PyQuery
 from GeoHealthCheck.plugin import Plugin
 from GeoHealthCheck.check import Check
 try:
@@ -9,78 +6,36 @@ except ImportError:
     from cgi import escape  # python 2.x
 
 #-# imported for this plugin #-#
-# import models
-import requests
+from pyquery import PyQuery
 import os
 import urllib
 import re
 
-from GeoHealthCheck.plugins.sla import TeamEngineAPI
+# User managment
+from GeoHealthCheck.models import User
 
+# For debugging
 from pdb import set_trace as bp
 import traceback
+# tmp: test workround for loading from this file
+# sys.path.insert(0,'/Users/forestg/dev/OGC/GeoHealthCheck')
 
-""" Contains basic Check classes for a Probe object."""
 
+"""
+Leverages the TEAM Engine project API to programatically test a service.
 
+@see: 
+http://opengeospatial.github.io/teamengine/index.html
+https://github.com/opengeospatial/teamengine
+"""
 class SlaOGCTestValidation(Check):
-    """
-    Leverages the TEAM Engine project API to programatically test a service.
 
-    @see: 
-        http://opengeospatial.github.io/teamengine/index.html
-        https://github.com/opengeospatial/teamengine
-    """
-
-    NAME = 'Runs tests in TEAM Engine test software. '
-    DESCRIPTION = 'Test the service for validation '
+    NAME = 'Team Engine Test Compliance'
+    DESCRIPTION = "Test resource against OGC's Team Engine"
 
     PARAM_DEFS = {
-        'TEAM Engine endpoint': {
-            'type': 'string',
-            'description': 'URL endpoint for the TEAM Engine service',
-            'default': 'http://cite.opengeospatial.org/teamengine/',
-            'required': True,
-            'range': None
-        },
-        'Test to Run': {
-            'type': 'string',
-            'description': 'What test would you like to run?',
-            # 'default': 'OGC Catalogue 3.0 Conformance Test Suite [cat30/1.0]',
-            'required': True,
-            'range': ['OGC Catalogue 3.0 Conformance Test Suite [cat30/1.0]',
-                    'GeoPackage 1.0 Conformance Test Suite [gpkg10/1.0]',
-                    'SensorThings API (STA) [sta10/1.0]',
-                    'WFS 2.0 (ISO 19142:2010) Conformance Test Suite [wfs20/1.26]',
-                    'KML 2.2 Conformance Test Suite [kml22/1.12]',
-                    'GML (ISO 19136:2007) Conformance Test Suite, Version 3.2.1 [gml32/1.25]']
-        },
-        'Frequency': {
-            'type': 'string',
-            'description': 'How often should we run tests to check compliance?',
-            'default': 'Every 3 Days',
-            'required': True,
-            'range': ['Every 1 Day', 
-                    'Every 3 Days', 
-                    'Every 1 Week', 
-                    'Every 2 Weeks', 
-                    'Every 1 Month']
-        },
-        'Time': {
-            'type': 'string',
-            'description': 'What time should we run the test?',
-            'default': '3:00AM',
-            'required': True,
-            'range': ['12:00PM', 
-                    '1:00AM',
-                    '2:00AM',
-                    '3:00AM',
-                    '4:00AM',
-                    '5:00AM',
-                    '6:00AM',
-                    '7:00AM',
-                    '8:00AM']
-        }
+        # Tests to omit?
+        # Something about not failing if test fails
     }
     """Param defs"""
 
@@ -88,79 +43,88 @@ class SlaOGCTestValidation(Check):
         Check.__init__(self)
 
     def perform(self):
-        # Wait until time matches -- Don't do now...
-        # Log in (or register) to TEAM Engine server
+        # TODO: Wait until time matches -- Don't do now...
+        bp()
 
-        # Call to TEAM Engine to run test
-        try :
+        uname = self.probe._resource.owner_identifier
+        owner = User.query.filter_by(username=uname).first()        
 
-            ##### For Testing (from file) #####
-            # lines = [];
-            # file = open(os.path.dirname(__file__) + "/../../../../results.xml")
-            # for line in file:
-            #     lines.append(line)
-            # 
-            # text = ''.join(lines).rstrip()
-            # self.set_result(True, text)
-            ###################################
-
-            ############ The Real deal ########
-            bp()
+        # Register user if they have not already been registerd in Team Engine
+        # try:
+        #     api = TeamEngineAPI(self.get_param('TEAM Engine endpoint'))
+        #     api.register(owner.username, owner.password)
+        #     api.authenticate(owner.username, owner.password)
 
 
-
-            te_test_endpoint = self.get_param('TEAM Engine endpoint')
-            te_test_name = self.get_param('Test to Run')
-            te_test_params = re.match(".+\[(.+)\]", te_test_name).groups()[0]
-            
-            # Must URL encode '&' chars per TEAM Engine documentation : 
-            #   http://opengeospatial.github.io/teamengine/users.html
-            resource_endpoint = self.probe.response.url.replace('&','%26')
-
-            try:
-                url = ("%s/rest/suites/%s/run?wfs=%s" % (te_test_endpoint, te_test_params, resource_endpoint)).replace('//rest','/rest')
-                cleanUrl = url.replace('https', 'http').replace('//rest','/rest')
-                print cleanUrl
-                resp = requests.get(cleanUrl)
-                text = resp.text
-
-                # If the test ran without issue
-                if resp.status_code == requests.codes.ok:
-                    doc = PyQuery(text.encode())
-                    failed = int(doc.attr('failed'))
-
-                    # report a pass or a fail
-                    if failed > 0 :
-                        self.set_result(False, text)
-                    else :
-                        self.set_result(True, text)
-
-                # Error happened running test
-                else:
-                    print text
-                    self.set_result(False, text)
-
-            except Exception as err:
-                print err
-                traceback.print_stack()
-                self.set_result(False, err)
-                return
-            ###################################
-
-            # bp()
+        #     # TODO: Run test here.....
 
 
+        # except Exception as err:
+        #     print err
+        #     traceback.print_stack()
 
-        except Exception as err: 
-            print(err)
-            self.set_result(False, err)
+        # ##### For Testing (from file) #####
+        # try :
+        #     lines = [];
+        #     file = open(os.path.dirname(__file__) + "/../../../../results.xml")
+        #     for line in file:
+        #         lines.append(line)
+
+        #     text = ''.join(lines).rstrip()
+        #     self.set_result(True, text)
+
+        # except Exception as err: 
+        #     print(err)
+        #     self.set_result(False, err)
+        ###################################
 
 
-# ASSUMPTION: this assumes that there is an hourly (or ever so often) check being run....
+        ############ The Real deal ########
+        # try:
+        #     te_test_endpoint = self.get_param('TEAM Engine endpoint')
+        #     te_test_name = self.get_param('Test to Run')
+        #     te_test_params = re.match(".+\[(.+)\]", te_test_name).groups()[0]
+        # 
+        #     # Must URL encode '&' chars per TEAM Engine documentation : 
+        #     #   http://opengeospatial.github.io/teamengine/users.html
+        #     resource_endpoint = self.probe.response.url.replace('&','%26')
+        # 
+        # 
+        #     url = ("%s/rest/suites/%s/run?wfs=%s" % (te_test_endpoint, te_test_params, resource_endpoint)).replace('//rest','/rest')
+        #     cleanUrl = url.replace('https', 'http').replace('//rest','/rest')
+        #     print cleanUrl
+        #     # resp = requests.get(cleanUrl)
+        #     # text = resp.text
+        # 
+        #     # If the test ran without issue
+        #     if resp.status_code == requests.codes.ok:
+        #         doc = PyQuery(text.encode())
+        #         failed = int(doc.attr('failed'))
+        # 
+        #         # report a pass or a fail
+        #         if failed > 0 :
+        #             self.set_result(False, text)
+        #         else :
+        #             self.set_result(True, text)
+        # 
+        #     # Error happened running test
+        #     else:
+        #         print text
+        #         self.set_result(False, text)
+        # 
+        # except Exception as err:
+        #     print err
+        #     traceback.print_stack()
+        #     self.set_result(False, err)
+        #     return
+        ###################################
+
+
+
+"""
+Check for verifying the uptime of a service
+"""
 class SlaVerifyUptime(Check):
-    """
-    Things
-    """
 
     NAME = 'Verify service reliability meets minimum requirements.'
     DESCRIPTION = 'Verify that service has been avaliable at a given minimum percentage rate.'
@@ -186,7 +150,7 @@ class SlaVerifyUptime(Check):
 
         if reliability < min_rel :
             self.set_result(False, "Service did not meet minimum SLA uptime requirement")
-      
+
 
 
 class SlaPreformant(Check):
@@ -214,46 +178,9 @@ class SlaPreformant(Check):
     def perform(self):
         result = True
         msg = 'OK'
+
+        # get record of uptime...
+        # get average
+        # see if its above the given average...
+        # 
         self.set_result(result, msg)
-
-
-    # ASSUMPTION: this assumes that there is an hourly (or ever so often) check being run....
-
-    # get record of uptime...
-    # get average
-    # see if its above the given average...
-
-
-class SLATestResultsHelper(object):
-    """
-    Helper class for reading and manipulating Test results saved from TEAM Engine
-    test suite runs.
-    """
-
-    def __init__(self, message):
-        self.RESP = message
-        try:
-            self.DOC = PyQuery(self.RESP.encode())
-        except Exception as err:
-            print err
-            self.DOC = None
-
-    def is_xml_response(self):
-        return self.DOC is not None
-
-    def get_passes(self):
-        if self.is_xml_response():
-            return self.DOC.find('[status="PASS"]')
-        else: 
-            return []
-
-
-    def get_failures(self):
-        if self.is_xml_response():
-            return self.DOC.find('[status="FAIL"]')
-        else: 
-            return []
-        
-    # Return default if empty
-    def show(self, obj, other): 
-        return obj if obj <> None else other  
--- GeoHealthCheck/plugins/probe/sla.py
+++ GeoHealthCheck/plugins/probe/sla.py
@@ -1,77 +1,298 @@
-import random
-
-# tmp: test workround for loading from this file
-import sys
-sys.path.insert(0,'/Users/forestg/dev/OGC/GeoHealthCheck')
-
+# -*- coding: utf-8 -*-
 from GeoHealthCheck.probe import Probe
 from GeoHealthCheck.result import Result
 from owslib.wms import WebMapService
 
-import traceback
 import requests
 import os
-from pyquery import PyQuery
+import urllib
 import re
 
+from pyquery import PyQuery
 
+# User managment
+from GeoHealthCheck.models import User
 
-class SLA(Probe):
+# For debugging
+from pdb import set_trace as bp
+import traceback
+
+# tmp: test workround for loading from this file
+# import sys
+# sys.path.insert(0,'/Users/forestg/dev/OGC/GeoHealthCheck')
+
+class SLA_Compliance(Probe):
     """
-    Probe for WMS endpoint "drilldown": starting
-    with GetCapabilities doc: get Layers and do
-    GetMap on them etc. Using OWSLib.WebMapService.
-
-    TODO: needs finalization.
+    SLA_Compliance:
+        This runs tests against the OGC Test Engine server to verify resource
+        compliance with standards.
     """
     AUTHOR = 'ImageMattersLLC Team'
-    NAME = 'SLA Check'
-    DESCRIPTION = 'Check for Compliance at a given Service Level Agreement (SLA).'
+    NAME = 'OGC Standards Compliance'
+    DESCRIPTION = 'Check for resource compliance against OGC standards test.'
     # See enums.py for complete list
     RESOURCE_TYPE = '*:*'
 
     REQUEST_METHOD = 'GET'
 
-    # PARAM_DEFS = {}
-
-    CHECKS_AVAIL = {
-        'GeoHealthCheck.plugins.check.checks.HttpStatusNoError': {
-            'default': True
+    PARAM_DEFS = {
+        'TEAM Engine endpoint': {
+            'type': 'string',
+            'description': 'URL endpoint for the TEAM Engine service',
+            'default': 'http://cite.opengeospatial.org/teamengine/',
+            'required': True,
+            'range': None
         },
-        'GeoHealthCheck.plugins.check.slachecks.SlaOGCTestValidation': {
-            'default': True
+        'Test to Run': {
+            'type': 'string',
+            'description': 'What test would you like to run?',
+            # 'default': 'OGC Catalogue 3.0 Conformance Test Suite [cat30/1.0]',
+            'required': True,
+            'range': ['OGC Catalogue 3.0 Conformance Test Suite [cat30/1.0]',
+                    'GeoPackage 1.0 Conformance Test Suite [gpkg10/1.0]',
+                    'SensorThings API (STA) [sta10/1.0]',
+                    'WFS 2.0 (ISO 19142:2010) Conformance Test Suite [wfs20/1.26]',
+                    'KML 2.2 Conformance Test Suite [kml22/1.12]',
+                    'GML (ISO 19136:2007) Conformance Test Suite, Version 3.2.1 [gml32/1.25]']
         },
-        'GeoHealthCheck.plugins.check.slachecks.SlaVerifyUptime': {
-            'default': True
-        },
-        # 'GeoHealthCheck.plugins.check.slachecks.SlaPreformant': {
-        #     'default': True
+        # 'Frequency': {
+        #     'type': 'string',
+        #     'description': 'How often should we run tests to check compliance?',
+        #     'default': 'Every 3 Days',
+        #     'required': True,
+        #     'range': ['Every 1 Day', 
+        #             'Every 3 Days', 
+        #             'Every 1 Week', 
+        #             'Every 2 Weeks', 
+        #             'Every 1 Month']
+        # },
+        # 'Time': {
+        #     'type': 'string',
+        #     'description': 'What time should we run the test?',
+        #     'default': '3:00AM',
+        #     'required': True,
+        #     'range': ['12:00PM', 
+        #             '1:00AM',
+        #             '2:00AM',
+        #             '3:00AM',
+        #             '4:00AM',
+        #             '5:00AM',
+        #             '6:00AM',
+        #             '7:00AM',
+        #             '8:00AM']
         # }
     }
-    """
-    Checks avail for all specific Caps checks.
-    Optionally override Check PARAM_DEFS using set_params
-    e.g. with specific `value`.
-    """
+
+    CHECKS_AVAIL = {
+        'GeoHealthCheck.plugins.check.slachecks.SlaOGCTestValidation': {
+            'default': True
+        }
+    }
 
     def __init__(self):
         Probe.__init__(self)
 
+    def perform_request(self):
+
+        uname = self._resource.owner_identifier
+        owner = User.query.filter_by(username=uname).first() 
+
+        # Register user if they have not already been registerd in Team Engine
+        try:
+            api = TeamEngineAPI(self.get_param('TEAM Engine endpoint'))
+            api.register(owner.username, owner.password)
+            api.authenticate(owner.username, owner.password)
+
+
+            # TODO: Run test here.....
+
+
+        except Exception as err:
+            print err
+            traceback.print_stack()
+
+
+        ######### Run the Test #########
+        result = Result(False, 'Guilty till proven innocent')
+        result.start() # start the timer
+
+        # ##### For Testing (from file) #####
+        try :
+            lines = [];
+            file = open(os.path.dirname(__file__) + "/../../../../results.xml")
+            for line in file:
+                lines.append(line)
+
+            text = ''.join(lines).rstrip()
+            result.set(True, text)
+
+        except Exception as err: 
+            print(err)
+            result.set(False, err)
+        ###################################
+
+        ############ The Real deal ############
+        # try:
+        #     te_test_endpoint = self.get_param('TEAM Engine endpoint')
+        #     te_test_name = self.get_param('Test to Run')
+        #     te_test_params = re.match(".+\[(.+)\]", te_test_name).groups()[0]
+        
+        #     # Must URL encode '&' chars per TEAM Engine documentation : 
+        #     #   http://opengeospatial.github.io/teamengine/users.html
+        #     resource_endpoint = self._resource.url.replace('&','%26')
+        
+        
+        #     url = ("%s/rest/suites/%s/run?wfs=%s" % (te_test_endpoint, te_test_params, resource_endpoint)).replace('//rest','/rest')
+        #     cleanUrl = url.replace('https', 'http').replace('//rest','/rest')
+  
+        #     print cleanUrl
+
+        #     resp = requests.get(cleanUrl)
+        #     result.set(True, resp.text)
+        #     bp()
+
+        # except Exception as err:
+        #     print err
+        #     traceback.print_stack()
+        #     result.set(False, err)
+        ###################################
+
+
+        finally:
+            result.stop()
+            self.result.add_result(result)
 
 
 
 
+
+# class SLA_Avalability(Probe):
+#     """
+#     SLA_Avalability:
+#         Run tests to make sure resource meets minimum uptime requirements.
+#     """
+#     AUTHOR = 'ImageMattersLLC Team'
+#     NAME = 'Avaliability check'
+#     DESCRIPTION = 'Check for resource meets minimum uptime requirements.'
+#     # See enums.py for complete list
+#     RESOURCE_TYPE = '*:*'
+
+#     REQUEST_METHOD = 'GET'
+
+#     # PARAM_DEFS = {}
+
+#     CHECKS_AVAIL = {
+#         'GeoHealthCheck.plugins.check.checks.HttpStatusNoError': {
+#             'default': True
+#         }
+#     }
+
+#     def __init__(self):
+#         Probe.__init__(self)
+
+
+
+# class SLA_Preformance(Probe):
+#     """
+#         SLA_Preformance:
+#         Tests a resource to make sure it meets minimum preformance requirements
+
+#         Standards are set as following:
+#         - Initial Response : max time 3 Seconds
+#          - 470 Kilobytes image (e.g. 800 × 600 color 8 bits): max time 5 seconds
+#         - Download Service Metadata operation: max time 10 seconds
+#         - Get Spatial Data Set/Get Spatial Object
+#             - initial response: max 30 seconds
+#             - maintain : 500 Spatial Objects per second
+#         - Describe Spatial Data Set/Describe Spatial Object Type
+#             - initial response: max 10 seconds
+#             - maintain 500 descriptions of Spatial Objects per second
+#     """
+#     AUTHOR = 'ImageMattersLLC Team'
+#     NAME = 'Preformace Check'
+#     DESCRIPTION = 'Check for preformace measurement of a resource'
+#     # See enums.py for complete list
+#     RESOURCE_TYPE = '*:*'
+
+#     REQUEST_METHOD = 'GET'
+
+#     # PARAM_DEFS = {}
+
+#     CHECKS_AVAIL = {
+#         # ...
+#     }
+
+#     def __init__(self):
+#         Probe.__init__(self)
+
+
+# class SLA_Capacity(Probe):
+    # NOT yet implemented
+
+
+
+
+
+
+
+
+"""
+Helper class for reading and manipulating Test results saved from TEAM Engine
+test suite runs.
+"""
+class SLATestResultsHelper(object):
+
+    def __init__(self, message):
+        self.RESP = message
+        try:
+            self.DOC = PyQuery(self.RESP.encode())
+        except Exception as err:
+            print err
+            self.DOC = None
+
+    def is_xml_response(self):
+        return self.DOC is not None
+
+    def get_passes(self):
+        if self.is_xml_response():
+            return self.DOC.find('[status="PASS"]')
+        else: 
+            return []
+
+
+    def get_failures(self):
+        if self.is_xml_response():
+            return self.DOC.find('[status="FAIL"]')
+        else: 
+            return []
+        
+    # Return default if empty
+    def show(self, obj, other): 
+        return obj if obj <> None else other  
+
+
+
+
+
+
+
+"""
+Helper class that allows for interacting with an OGC Team Engine server
+
+Team Engine:
+    http://opengeospatial.github.io/teamengine/
+"""
 class TeamEngineAPI(object):
-    
+
     ENDPOINT = None
     UNAME = None
     password = None
     COOKIEJAR = None
-    
+
     def __init__(self, endpoint):
         self.ENDPOINT = endpoint
         self.touch()
-        
+
     def saveCookies(self, resp):
         self.COOKIEJAR = resp.cookies
 
@@ -84,9 +305,8 @@ class TeamEngineAPI(object):
                     ('username', uname), \
                     ('password', password), \
                     ('repeat_password', password))
-        resp = requests.post(self.ENDPOINT + 'registrationHandler', data=payload, cookies=self.COOKIEJAR)
-        self.saveCookies(resp)
-        return resp
+        # NOTE: we do not save cookie after registration as it prevents immediate login
+        return requests.post(self.ENDPOINT + 'registrationHandler', data=payload, cookies=self.COOKIEJAR)
 
     def authenticate(self, uname, password):
         payload = (('j_username', uname),('j_password', password))
@@ -116,6 +336,8 @@ class TeamEngineAPI(object):
     # def getLatestTestResult(self):
     # self.listTestResults(self) #...
     # Will have to filter by somthing here.... 
+
+
 
 
 
